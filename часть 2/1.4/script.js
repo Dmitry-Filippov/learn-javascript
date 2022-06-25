2765499401365f019d3e09cde523618a971be628
Dmitry Filippov <demodima97@mail.ru>
2022-06-25 09:26:34 +0300

    1.4

---
+++ часть 2/1.4/script.js
@@ -0,0 +1,9 @@
+const table = document.getElementById("age-table");
+const labels = document.querySelectorAll("#age-table label");
+const td = document.querySelector("td");
+const form = document.getElementsByName("search")[0];
+const firstInput = document.querySelector("input");
+const lastInput =
+  document.querySelectorAll("input")[
+    document.querySelectorAll("input").length - 1
+  ];
